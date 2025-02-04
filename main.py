17b6fd83c2888e5e0c99a13dbc241414d3d58305
keala <K13DOHERTY@student.bridgew.edu>
2025-02-03 17:38:26 -0800

    final verision for sprint 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import google.generativeai as genai  # CODE BELOW FOR THE MOST PART WAS TAKEN FR
 
 
 def main():
-    genai.configure(api_key= private.API_KEY)
+    genai.configure(api_key=private.API_KEY)
 
     # Create the model
 
@@ -16,8 +16,8 @@ def main():
     }
 
     model = genai.GenerativeModel(
-      model_name = "gemini-2.0-flash-exp",
-      generation_config = generation_config,
+      model_name="gemini-2.0-flash-exp",
+      generation_config=generation_config,
     )
 
     chat_session = model.start_chat(
@@ -26,18 +26,18 @@ def main():
     )
 
     with open('rapid_jobs2.json') as json_data:
-      data = json_data.read()
+        data = json_data.read()
     prompt = f"""create a sample resume for a junior software engineer and make the resume in markdown format and for th
-              e ""jobs in:{data}""" # prompt to generate the LLM for the generated resume
-    response = chat_session.send_message(prompt) ## generates the resume
+              e ""jobs in:{data}"""  # prompt to generate the LLM for the generated resume
+    response = chat_session.send_message(prompt) # generates the resume
 
     print("Resume has been generated transferring resume to a text File now")
     generated_resume = response.text
 
-    with open('resume.txt', 'w') as file: # creates the txt file and writes the resume to the txt file.
-      file.write(generated_resume)
+    with open('resume.txt', 'w') as file:  # creates the txt file and writes the resume to the txt file.
+        file.write(generated_resume)
 
     print("Resume has been transferred to the text file under resume.txt")
 
     if __name__ == "__main__":
-      main()
+        main()
