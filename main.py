b53be585eeae84edc0b6f7d44a8947b4373cac6b
keala <K13DOHERTY@student.bridgew.edu>
2025-02-06 19:59:38 -0800

    database and table are created next push will be for when json job data has been loaded within the database.

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import private
 import google.generativeai as genai  # CODE BELOW FOR THE MOST PART WAS TAKEN FROM PROVIDED CODE FROM GOOGLE AI
+import sqlite3
+import json
+from typing import Tuple
+
+
+def open_database(filename:str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    db_connection = sqlite3.connect(filename)
+    cursor = db_connection.cursor()
+    return db_connection, cursor
+
+
+def close_database(connection:sqlite3.Connection):
+    connection.commit()
+    connection.close()
+
+
+def read_data(filename, data):
+    with open('rapid_jobs2.json', 'r') as x:
+        data = json.load(x)
+        return data
+
+
+def set_db_table(cursor:sqlite3.Cursor):
+    table = '''CREATE TABLE JOB_DATA(
+                JOB_TITLE TEXT PRIMARY KEY,
+                JOB_COMPANY TEXT,
+                JOB_LOCATION TEXT,
+                JOB_DESCRIPTION TEXT,
+                JOB_QUALIFICATIONS TEXT,
+                JOB_SALARY INT 
+                );'''
+    cursor.execute(table)
 
 
 def main():
 
-    genai.configure(api_key=private.API_KEY)
+    data = {}
+    conn, cursor = open_database('jobs_db.sqlite')
+    set_db_table(cursor)
+    conn.commit()
+    conn.close()
+    genai.configure(api_key=private.API_KEY) # Import API key
     # Create the model
 
     generation_config = {
@@ -25,8 +62,6 @@ def main():
       ]
     )
 
-    with open('rapid_jobs2.json') as json_data:
-        data = json_data.read()
     prompt = f"""create a sample resume for a junior software engineer and make the resume in markdown format and for th
               e ""jobs in:{data}"""  # prompt to generate the LLM for the generated resume
     response = chat_session.send_message(prompt)  # generates the resume
