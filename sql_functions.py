b5504db512351aa2ba2353e78f30c09e52d8c1b8
keala <K13DOHERTY@student.bridgew.edu>
2025-02-10 17:14:45 -0800

    uploading remaining sprint 2 files to lint them

--- sql_functions.py
+++ sql_functions.py
@@ -15,33 +15,33 @@ def close_database(connection: sqlite3.Connection):  # closes database
 
 def set_rapid_db(cursor: sqlite3.Cursor):
     table = '''CREATE TABLE IF NOT EXISTS JOB_DATA(
-    JOB_ID TEXT PRIMARY KEY, 
-    JOB_TITLE TEXT ,
-    JOB_COMPANY TEXT,
-    JOB_DESCRIPTION TEXT,
-    JOB_IMAGE TEXT,
-    JOB_LOCATION TEXT,
-    JOB_EMPLOYMENT TEXT,
-    JOB_DATE_POSTED TEXT,
-    JOB_SALARY INT,
-    JOB_PROVIDER TEXT);'''
+            JOB_ID TEXT PRIMARY KEY, 
+            JOB_TITLE TEXT ,
+            JOB_COMPANY TEXT,
+            JOB_DESCRIPTION TEXT,
+            JOB_IMAGE TEXT,
+            JOB_LOCATION TEXT,
+            JOB_EMPLOYMENT TEXT,
+            JOB_DATE_POSTED TEXT,
+            JOB_SALARY INT,
+            JOB_PROVIDER TEXT);'''
     cursor.execute(table)
 
 
 def set_results_db(cursor: sqlite3.Cursor):
     table = '''CREATE TABLE IF NOT EXISTS RAPID_JOB_DATA(
-    JOB_ID TEXT PRIMARY KEY, 
-    JOB_SITE TEXT,
-    JOB_URL TEXT,
-    JOB_TITLE TEXT,
-    JOB_COMPANY TEXT,
-    JOB_LOCATION TEXT,
-    JOB_TYPE TEXT,
-    JOB_DATE_POSTED TEXT,
-    JOB_SALARY_INTERVAL TEXT,
-    JOB_SALARY_MIN INT,
-    JOB_SALARY_MAX INT,
-    JOB_IS_REMOTE TEXT,
-    JOB_EMAILS TEXT,
-    JOB_DESCRIPTION TEXT);'''
+            JOB_ID TEXT PRIMARY KEY, 
+            JOB_SITE TEXT,
+            JOB_URL TEXT,
+            JOB_TITLE TEXT,
+            JOB_COMPANY TEXT,
+            JOB_LOCATION TEXT,
+            JOB_TYPE TEXT,
+            JOB_DATE_POSTED TEXT,
+            JOB_SALARY_INTERVAL TEXT,
+            JOB_SALARY_MIN INT,
+            JOB_SALARY_MAX INT,
+            JOB_IS_REMOTE TEXT,
+            JOB_EMAILS TEXT,
+            JOB_DESCRIPTION TEXT);'''
     cursor.execute(table)
